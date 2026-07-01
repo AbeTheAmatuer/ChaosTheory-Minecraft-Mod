ea44b8ac93d9892dcb16061c0af96d0b14154827
Sir Abe <ibrahimkhurram2008@gmail.com>
2026-07-01 16:01:05 +0500

    First commit
    hello world

---
+++ src/client/java/abe/chaostheory/client/ChaosTheoryDataGenerator.java
@@ -0,0 +1,11 @@
+package abe.chaostheory.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChaosTheoryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
